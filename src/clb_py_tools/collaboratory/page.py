f5dd5ea32947dfc43858e338a7c6055ce000eb19
Jonathan Villemaire-Krajden <jonathan.villemaire-krajden@epfl.ch>
2020-03-12 17:28:38 +0100

    Add some caching, fix page names with colons.

--- src/clb_py_tools/collaboratory/page.py
+++ src/clb_py_tools/collaboratory/page.py
@@ -1,4 +1,5 @@
 # ~*~ coding: utf-8 ~*~
+from datetime import datetime, timedelta
 import typing
 
 from clb_py_tools import collaboratory
@@ -7,16 +8,19 @@ from clb_py_tools import collaboratory
 class Page:
     """Represents an xwiki page.
 
-    :ivar name: Page name: the last url segment of the collab, set in the collab properties. This is the last space's name in XWiki, rather than WebHome. Might not work with terminal pages.
+    :ivar name: Page name: the last url segment of the collab, set in the collab properties.
+                This is the last space's name in XWiki, rather than WebHome. Might not work
+                with terminal pages.
     :ivar title: The page title.
     :ivar content: The content of this page. This is unrendered XWiki markup.
     :ivar author: The creator of the collab.
     :ivar created_at: The date and time of creation.
 
-    .. Note:: The content is not loaded when the pages are listed. You need to call `.refresh()` on the page instance first.
+    .. Note:: The content is not loaded when the pages are listed. You need to call
+              `.refresh()` on the page instance first.
     """
     _properties = ('author', 'content', 'version', 'title', 'created_at',
-                   'modified_at', 'space')
+                   'modified_at', 'space', 'links')
     _property_map = {'modified_at': 'modifiedAt', 'created_at': 'createdAt'}
 
     """ An object representing an XWiki page in the Collab """
@@ -26,7 +30,11 @@ class Page:
         :param parent_: a collaboratory.Page object under which this page is nested.
         """
         self._parent = parent_
-        self._pages = None
+        self._pages: typing.Dict[str, "Page"] = {}
+        self._pages_cached_at = datetime.min
+        self.space = ''
+        self.content = ''
+        self.title = ''
         self.name = name
         self.page_name = 'WebHome'
         self.load_values(kwargs)
@@ -41,17 +49,17 @@ class Page:
         self._relative_url = self._parent._relative_url + f'/spaces/{self.name}'
         self._webhome_url = self._relative_url + f'/pages/{self.page_name}'
 
-    def _fix_page_name(self) -> str:
+    def _fix_page_name(self) -> None:
         """Fix the name when initialising from the xwiki representation.
 
         Uses the space name rather than WebHome.
 
         Note: Might break things a bit if someone creates a terminal page.
         """
-        if self.space:
+        if getattr(self, 'space', None):
             self.page_name = self.name
             # Find the parent space's name
-            self.name = self.space[self.space.rindex('.')+1:]
+            self.name = self.space[self.space.rindex('.')+1:].replace('\\:', ':')
 
     def export_values(self) -> typing.Dict[str, str]:
         """ Rerturn a dictionnary with page content. """
@@ -82,15 +90,30 @@ class Page:
         values = self._collaboratory.get(self._webhome_url)
         self.load_values(values)
 
+    def rest_href(self):
+        try:
+            return [link['href'] for link in self.links if link['rel'] ==
+                    "http://www.xwiki.org/rel/page"]
+        except KeyError:
+            # @TODO
+            raise Exception("Missing link")
+
     @property
-    def pages(self) -> typing.Dict[str, "Pages"]:
+    def pages(self) -> typing.Dict[str, "Page"]:
         """ List the pages under this page.
 
         Note: This will ignore missing levels with grandchildren
+        Caches for 45 seconds.
         """
-        if self._pages is None:
+        now = datetime.now()
+        if now - timedelta(seconds=45) > self._pages_cached_at:
+            # set it before trying so if it fails, it won't retry for
+            # the cache period. It's short anyways and will protect
+            # from an accidental DOS
+            self._pages_cached_at = now
             resp = self._collaboratory.get(self._webhome_url + '/children')
             pages_info = resp['pageSummaries']
             pages_ = [Page(self, **page_info) for page_info in pages_info]
             self._pages = {page.name: page for page in pages_}
+
         return self._pages
